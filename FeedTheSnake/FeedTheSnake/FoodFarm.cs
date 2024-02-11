e610140aead1d222ee8ee42cc5d3aec2d45eb6ca
Galin Todorov <galin.todorov@payhawk.com>
2024-02-11 14:17:28 +0200

    revert to 5

--- FeedTheSnake/FeedTheSnake/FoodFarm.cs
+++ FeedTheSnake/FeedTheSnake/FoodFarm.cs
@@ -46,7 +46,7 @@ namespace FeedTheSnake
         #region Constructors
         public FoodFarm(double foodRadius, int capasity, int productionTime)
         {
-            ExpiretionTime = 7;
+            ExpiretionTime = 5;
             CurrentTime = 0;
             FoodRadius = foodRadius;
             Capasity = capasity;
